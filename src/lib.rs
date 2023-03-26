c2a20cb652503d32a7ebeac71cd464bd8aefbc46
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2023-03-26 15:51:01 -0600

    create second menu and add callbacks to declaratively define behavior

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@ impl Plugin for MenuDemoGame {
             .add_system(setup_camera.on_startup())
             .add_system(setup_main_menu.in_schedule(OnEnter(GameState::MainMenu)))
             .add_system(cleanup_main_menu.in_schedule(OnExit(GameState::MainMenu)))
+            .add_system(setup_second_menu.in_schedule(OnEnter(GameState::SecondMenu)))
+            .add_system(cleanup_second_menu.in_schedule(OnExit(GameState::SecondMenu)))
             .add_system(button_system);
     }
 }
@@ -24,12 +26,37 @@ fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-#[derive(Component)]
-struct MainMenu;
+// TODO: extract ui code into separate modules
+// TODO: consider creating a macro so I can register systems of arbitrary types for button callbacks (such as a possible state Res<...>)
+// TODO: change color of button interaction based on luminance values (low luminance goes up, high luminance goes down)
 
 const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
 const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
 const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.35, 0.35);
+
+#[derive(Component)]
+struct OnPressed(OnPressedFn);
+type OnPressedFn = Box<dyn Fn(&mut ResMut<NextState<GameState>>) + Send + Sync>;
+
+fn button_system(
+    mut button_interaction_query: ButtonInteractionQuery,
+    mut next_state: ResMut<NextState<GameState>>,
+) {
+    for (interaction, on_interacted, mut color) in &mut button_interaction_query {
+        let new_color = match interaction {
+            Interaction::None => NORMAL_BUTTON,
+            Interaction::Hovered => HOVERED_BUTTON,
+            Interaction::Clicked => {
+                on_interacted.0(&mut next_state);
+                PRESSED_BUTTON
+            }
+        };
+        *color = new_color.into();
+    }
+}
+
+#[derive(Component)]
+struct MainMenu;
 
 fn setup_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
@@ -48,20 +75,26 @@ fn setup_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
         ))
         .with_children(|parent| {
             parent
-                .spawn(ButtonBundle {
-                    background_color: NORMAL_BUTTON.into(),
-                    style: Style {
-                        size: Size::new(Val::Px(150.0), Val::Px(65.0)),
-                        justify_content: JustifyContent::Center,
-                        align_items: AlignItems::Center,
+                .spawn((
+                    OnPressed(Box::new(|next_state| {
+                        println!("pressed main button");
+                        next_state.set(GameState::SecondMenu);
+                    })),
+                    ButtonBundle {
+                        background_color: NORMAL_BUTTON.into(),
+                        style: Style {
+                            size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::Center,
+                            ..default()
+                        },
                         ..default()
                     },
-                    ..default()
-                })
+                ))
                 .with_children(|parent| {
                     parent.spawn(TextBundle {
                         text: Text::from_section(
-                            "Button",
+                            "Go to Second Menu",
                             TextStyle {
                                 font: asset_server.load("fonts/FiraSans-Bold.ttf"),
                                 font_size: 40.0,
@@ -80,21 +113,67 @@ fn cleanup_main_menu(mut commands: Commands, query: Query<Entity, With<MainMenu>
     }
 }
 
-fn button_system(
-    mut button_interaction_query: ButtonInteractionQuery,
-    mut next_state: ResMut<NextState<GameState>>,
-) {
-    for (interaction, mut color) in &mut button_interaction_query {
-        let new_color = match interaction {
-            Interaction::None => NORMAL_BUTTON,
-            Interaction::Hovered => HOVERED_BUTTON,
-            Interaction::Clicked => {
-                next_state.set(GameState::SecondMenu);
-                PRESSED_BUTTON
-            }
-        };
-        *color = new_color.into();
+type ButtonInteractionQuery<'a, 'b, 'c> = Query<
+    'a,
+    'b,
+    (&'c Interaction, &'c OnPressed, &'c mut BackgroundColor),
+    (Changed<Interaction>, With<Button>),
+>;
+
+#[derive(Component)]
+struct SecondMenu;
+
+fn setup_second_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands
+        .spawn((
+            SecondMenu,
+            NodeBundle {
+                background_color: Color::GRAY.into(),
+                style: Style {
+                    size: Size::width(Val::Percent(100.0)),
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::Center,
+                    ..default()
+                },
+                ..default()
+            },
+        ))
+        .with_children(|parent| {
+            parent
+                .spawn((
+                    OnPressed(Box::new(|next_state| {
+                        println!("pressed second button");
+                        next_state.set(GameState::MainMenu);
+                    })),
+                    ButtonBundle {
+                        background_color: NORMAL_BUTTON.into(),
+                        style: Style {
+                            size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::Center,
+                            ..default()
+                        },
+                        ..default()
+                    },
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle {
+                        text: Text::from_section(
+                            "Go to Main Menu",
+                            TextStyle {
+                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                font_size: 40.0,
+                                color: Color::rgb(0.9, 0.9, 0.9),
+                            },
+                        ),
+                        ..default()
+                    });
+                });
+        });
+}
+
+fn cleanup_second_menu(mut commands: Commands, query: Query<Entity, With<SecondMenu>>) {
+    for entity in &query {
+        commands.entity(entity).despawn_recursive();
     }
 }
-type ButtonInteractionQuery<'a, 'b, 'c> =
-    Query<'a, 'b, (&'c Interaction, &'c mut BackgroundColor), (Changed<Interaction>, With<Button>)>;
