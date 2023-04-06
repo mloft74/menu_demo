b09db26ed568990b935d5324ce69c67236543858
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2023-04-06 14:43:02 -0600

    Add an exit button to the main menu

--- src/main_menu.rs
+++ src/main_menu.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{app::AppExit, prelude::*};
 use bevy_egui::{egui, EguiContexts};
 
 use crate::{ui::UiDefault, GameState};
@@ -11,18 +11,27 @@ impl Plugin for MainMenu {
     }
 }
 
-fn main_menu(mut contexts: EguiContexts, mut next_state: ResMut<NextState<GameState>>) {
+fn main_menu(
+    mut contexts: EguiContexts,
+    mut next_state: ResMut<NextState<GameState>>,
+    mut exit: EventWriter<AppExit>,
+) {
     egui::CentralPanel::default()
         .frame(egui::Frame::ui_default())
         .show(contexts.ctx_mut(), |ui| {
             ui.vertical_centered(|ui| {
-                let padding = 12.0;
-                ui.add_space(padding);
+                let padding_l = 12.0;
+                let padding_s = 8.0;
+                ui.add_space(padding_l);
                 ui.heading("Main Menu");
                 ui.separator();
-                ui.add_space(padding);
+                ui.add_space(padding_l);
                 if ui.button("Start").clicked() {
                     next_state.set(GameState::GamePlay);
+                }
+                ui.add_space(padding_s);
+                if ui.button("Exit").clicked() {
+                    exit.send(AppExit);
                 }
             });
         });
